113e4d6a1c4cf414a32c359bec08868ac183dd2b
Leo <lnddru@gmail.com>
2019-12-01 17:47:44 -0300

    Add files via upload

---
+++ estudo01.py
@@ -0,0 +1,10 @@
+# -*- coding: UTF-8 -*-
+
+import pandas as pl
+import matplotlib.pyplot as plt
+
+
+df = pl.read_csv('questionario.csv')
+
+with open('questionario.txt', 'w') as f:
+    df.to_string(f)
---
+++ teste02.py
@@ -0,0 +1,22 @@
+# -*- coding: UTF-8 -*-
+
+a = [1, 2, 3]
+
+a.append(15)
+
+print(a)
+
+
+b = [7, 8, 9 ]
+
+"""  
+
+     append -> para adicionar um item na lista
+     neste exercicio adicionamos os itens do elemento A  na lista B
+
+
+    """
+
+for item in b:
+	a.append(item)
+print(a)	
